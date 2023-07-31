db314f2aa4857081c14771982927eccc007f6ef3
Matt Kindig <mkindig97@gmail.com>
2023-07-31 17:15:03 -0400

    This is the finished product for workshop 27.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  const [token, setToken] = useState(null);
+  return (
+    <>
+      <Authenticate token={token} setToken={setToken} />
+      <SignUpForm token={token} setToken={setToken}/>
+    </>
+  );
+}
+
+
